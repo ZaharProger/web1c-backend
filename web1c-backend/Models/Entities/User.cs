93bd8adaaf400f27638fb5245000247b8b0bfdeb
Семен Севастеев <kong01@bk.ru>
2022-11-26 12:56:28 +0800

    User add get/set

--- web1c-backend/Models/Entities/User.cs
+++ web1c-backend/Models/Entities/User.cs
@@ -2,5 +2,13 @@
 {
     public class User
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity), Key, Column("En_user_id")]
+        public long En_user_id { get; set; }
+
+        [Column("En_user_login", TypeName = "varchar(50")]
+        public string En_user_login { get; set; }
+
+        [Column("En_user_password", TypeName = "varbinary(100")]
+        public byte[] En_user_password { get; set; }
     }
 }
